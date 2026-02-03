b4e0f0d5183292636e7b770e0a3bf2c1ea02f62e
Oleh Pona <olehpona@gmail.com>
2026-02-03 17:19:34 +0200

    added logger

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module arr_mapper
 
 go 1.25.6
+
+require (
+	github.com/sirupsen/logrus v1.9.4 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 type MovieData struct {
@@ -17,7 +18,7 @@ type RequestBody struct {
 	EventType    *string    `json:"eventType"`
 	InstanceName *string    `json:"instanceName"`
 	Movie        *MovieData `json:"movie"`
-	DownloadId   *string     `json:"downloadId"`
+	DownloadId   *string    `json:"downloadId"`
 	DeleteFiles  bool       `json:"deletedFiles"`
 }
 
@@ -26,31 +27,46 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&parsedBody)
 	if err != nil || parsedBody.EventType == nil || parsedBody.Movie == nil || parsedBody.InstanceName == nil {
-		fmt.Printf("JSON validation error: %v\n", err)
+		logrus.Errorf("JSON validation error: %v", err)
 		w.WriteHeader(400)
 		return
 	}
 
 	if *parsedBody.EventType == "Grab" && parsedBody.DownloadId != nil {
+		logrus.Infof("Adding movie %d from instance %s with torrent hash %s", parsedBody.Movie.ID, *parsedBody.InstanceName, *parsedBody.DownloadId)
 		db.AddMovie(MapData{
 			Instance:    *parsedBody.InstanceName,
 			InstanceId:  parsedBody.Movie.ID,
 			TorrentHash: strings.ToLower(*parsedBody.DownloadId),
 		})
 	} else if *parsedBody.EventType == "MovieDelete" {
+		logrus.Infof("Removing movie %d from instance %s, delete files: %t", parsedBody.Movie.ID, *parsedBody.InstanceName, parsedBody.DeleteFiles)
 		db.RemoveMovie(*parsedBody.InstanceName, parsedBody.Movie.ID, parsedBody.DeleteFiles)
 	}
 	w.WriteHeader(200)
 }
 
+func initLogger() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.WarnLevel)
+}
+
 func main() {
-	db.Load()
+	initLogger()
+	err := db.Load()
+	if err != nil {
+		logrus.Errorf("error loading database: %s", err)
+	}
 	http.HandleFunc("/", handleRequest)
-	err := http.ListenAndServe(":9191", nil)
+	logrus.Warnf("Server running on port 9191")
+	err = http.ListenAndServe(":9191", nil)
 	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
+		logrus.Infof("server closed")
 	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
+		logrus.Errorf("error starting server: %s", err)
 		os.Exit(1)
 	}
 }
